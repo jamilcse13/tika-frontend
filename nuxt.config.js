f21c212d01cdd002faf91c6a8c3bc92130946542
jamil <jamil.cse13@gmail.com>
2022-02-02 01:31:06 +0600

    setup axios ans hit api from frontend

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,12 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/axios',
   ],
+
+  axios: {
+  baseURL: 'http://127.0.0.1:8000/api'
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,29 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1 v-if="apiValue">Check: {{ apiValue }}</h1>
+    <button @click.prevent="testBackendRequest">Request Api</button>
+    <Tutorial/>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  data() {
+    return {
+      apiValue: ''
+    }
+  },
+
+  mounted() {
+
+  },
+
+  methods: {
+    testBackendRequest() {
+      this.$axios.get('/test').then(res => {
+        this.apiValue = res.data;
+      })
+    }
+  }
 }
 </script>
